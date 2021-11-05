ecf2458e443d20c47e22b2a9a17c5a19af502fc5
Amalia <amalialorenaferariu@gmail.com>
2021-11-05 14:54:49 +0100

    step1

--- main.js
+++ main.js
@@ -16,3 +16,46 @@
 // 3. Creo l'array di oggetti
 // 4. Genero il template con i dati dell'array
 // 5. Aggiungo funzionalità click
+
+let profilePic = "https://unsplash.it/300/300?image=15";
+let author = "Phil Mangione";
+let image = "https://unsplash.it/600/300?image=171";
+let text = "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias."
+let date = "4 mesi fa";
+let likes = 80;
+
+let template = `<div class="post">
+<div class="post__header">
+    <div class="post-meta">                    
+        <div class="post-meta__icon">
+            <img class="profile-pic" src="${profilePic}" alt="Phil Mangione">                    
+        </div>
+        <div class="post-meta__data">
+            <div class="post-meta__author">${author}</div>
+            <div class="post-meta__time">${date}</div>
+        </div>                    
+    </div>
+</div>
+<div class="post__text">${text}</div>
+<div class="post__image">
+    <img src="${image}" alt="">
+</div>
+<div class="post__footer">
+    <div class="likes js-likes">
+        <div class="likes__cta">
+            <a class="like-button  js-like-button" href="#" data-postid="1">
+                <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                <span class="like-button__label">Mi Piace</span>
+            </a>
+        </div>
+        <div class="likes__counter">
+            Piace a <b id="like-counter-1" class="js-likes-counter">${likes}</b> persone
+        </div>
+    </div> 
+</div>            
+</div>`;
+
+let container = document.getElementById("container");
+
+container.innerHTML = template;
+
