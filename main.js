b04b2418ca4db44060a9949d986740bcf76bf0a1
Amalia <amalialorenaferariu@gmail.com>
2021-11-05 15:12:45 +0100

    template function

--- main.js
+++ main.js
@@ -7,9 +7,8 @@
 // testo del post,
 // immagine (non tutti i post devono avere una immagine) [quindi gestisco il caso],
 // numero di likes.
-// - Prendendo come riferimento il layout di esempio presente nell’html, stampiamo i post del nostro feed.    
+// - Prendendo come riferimento il layout di esempio presente nell’html, stampiamo i post del nostro feed.
 // - Rendiamo il tasto “Mi Piace” cliccabile con incremento del counter dei likes.
-
 
 // 1. Trasformo il template html in elementi js
 // 2. Rendo dinamico il template
@@ -17,47 +16,50 @@
 // 4. Genero il template con i dati dell'array
 // 5. Aggiungo funzionalità click
 
-let profilePic = "https://unsplash.it/300/300?image=15";
-let author = "Phil Mangione";
-let image = "https://unsplash.it/600/300?image=171";
-let text = "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias."
-let date = "4 mesi fa";
-let likes = 80;
+let user = {
+  profilePic: "https://unsplash.it/300/300?image=15",
+  author: "Phil Mangione",
+  image: "https://unsplash.it/600/300?image=171",
+  text: "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+  date: "4 mesi fa",
+  likes: 80,
+};
 
-function createTemplate (profilePic, author, date, text, image, likes) {
-return `<div class="post">
-<div class="post__header">
-    <div class="post-meta">                    
-        <div class="post-meta__icon">
-            <img class="profile-pic" src="${profilePic}" alt="Phil Mangione">                    
+function createTemplate(user) {
+  const { profilePic, author, date, text, image, likes } = user;
+  return `
+    <div class="post">
+        <div class="post__header">
+            <div class="post-meta">                    
+                <div class="post-meta__icon">
+                    <img class="profile-pic" src="${profilePic}" alt="Phil Mangione">                    
+                </div>
+                <div class="post-meta__data">
+                    <div class="post-meta__author">${author}</div>
+                    <div class="post-meta__time">${date}</div>
+                </div>                    
+            </div>
         </div>
-        <div class="post-meta__data">
-            <div class="post-meta__author">${author}</div>
-            <div class="post-meta__time">${date}</div>
-        </div>                    
-    </div>
-</div>
-<div class="post__text">${text}</div>
-<div class="post__image">
-    <img src="${image}" alt="">
-</div>
-<div class="post__footer">
-    <div class="likes js-likes">
-        <div class="likes__cta">
-            <a class="like-button  js-like-button" href="#" data-postid="1">
-                <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
-                <span class="like-button__label">Mi Piace</span>
-            </a>
+        <div class="post__text">${text}</div>
+        <div class="post__image">
+            <img src="${image}" alt="">
         </div>
-        <div class="likes__counter">
-            Piace a <b id="like-counter-1" class="js-likes-counter">${likes}</b> persone
-        </div>
-    </div> 
-</div>            
-</div>`;
-} 
+        <div class="post__footer">
+            <div class="likes js-likes">
+                <div class="likes__cta">
+                    <a class="like-button  js-like-button" href="#" data-postid="1">
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </a>
+                </div>
+                <div class="likes__counter">
+                    Piace a <b id="like-counter-1" class="js-likes-counter">${likes}</b> persone
+                </div>
+            </div> 
+        </div>            
+    </div>`;
+}
 
 let container = document.getElementById("container");
 
-container.innerHTML = createTemplate(profilePic, author, date, text, image, likes);
-
+container.innerHTML = createTemplate(user);
