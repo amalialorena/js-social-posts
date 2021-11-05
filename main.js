55fa59908d1bf6b32f99e4af9b9624ae2a8228e0
Amalia <amalialorenaferariu@gmail.com>
2021-11-05 14:26:44 +0100

    init

---
+++ main.js
@@ -0,0 +1,11 @@
+// Ricreiamo un feed social aggiungendo al layout di base fornito, il nostro javascript in cui:
+// - Creiamo il nostro array di oggetti che rappresentano ciascun post.
+// Ogni post dovrà avere le informazioni necessarie per stampare la relativa card:
+// nome autore,
+// foto profilo,
+// data,
+// testo del post,
+// immagine (non tutti i post devono avere una immagine) [quindi gestisco il caso],
+// numero di likes.
+// - Prendendo come riferimento il layout di esempio presente nell’html, stampiamo i post del nostro feed.
+// - Rendiamo il tasto “Mi Piace” cliccabile con incremento del counter dei likes.
